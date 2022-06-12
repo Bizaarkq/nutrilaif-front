b4b9b2bec2e3897f8fc11fcc954b38b84c92a2df
Bizaarkq <isaac.bernab.01@gmail.com>
2022-06-11 22:14:43 -0600

    🐛 fix disabled para editar alimento

--- src/app/views/components/dialog-alimento/dialog-alimento.component.ts
+++ src/app/views/components/dialog-alimento/dialog-alimento.component.ts
@@ -23,12 +23,14 @@ export class DialogAlimentoComponent implements OnInit {
     'calcio',
     'sodio'
   ];
+  codigoDisabled:boolean = false;
   
   ngOnInit(): void {
+    this.codigoDisabled = this.editData ? true : false;
     //this.createForm();
     //Validar campos del formulario
     this.formDatosAlimento = this.fb.group({
-      codigo: [{value:'', disabled:true}, Validators.required],
+      codigo: [{value:'', disabled: this.codigoDisabled}, Validators.required],
       nombre: ['', Validators.required],
       calorias: ['', Validators.required],
       grasas: ['', Validators.required],
