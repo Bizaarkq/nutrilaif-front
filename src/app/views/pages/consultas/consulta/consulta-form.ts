dafd2cd08f05b302b563fc4de9c789af4c201e75
otrodesconocidomas <rv17012@ues.edu.sv>
2022-07-08 22:10:10 -0600

    ✨ validaciones

--- src/app/views/pages/consultas/consulta/consulta-form.ts
+++ src/app/views/pages/consultas/consulta/consulta-form.ts
@@ -1,5 +1,6 @@
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Injectable } from "@angular/core";
+import { validateHorizontalPosition } from "@angular/cdk/overlay";
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +12,7 @@ export class ConsultaForm{
     switch(option){
       case "datos_medicos":
           form = this.FB.group({
-          diagnostico: [''],
+          diagnostico: ['',[Validators.required]],
           medicamento_suplemento: [''],
           otros_datos: ['']
         });
@@ -34,19 +35,19 @@ export class ConsultaForm{
         break;
       case "datos_antropo":
           form = this.FB.group({
-          peso_actual: [''],
-          peso_ideal: [''],
-          p_grasa_corporal: [''],
-          p_masa_muscular: [''],
-          p_grasa_visceral: [''],
-          peso_meta: [''],
-          talla: [''],
-          c_cintura: [''],
-          imc: [''],
-          edad_metabolica: [''],
-          c_brazo_relaj: [''],
-          c_cadera: [''],
-          c_muneca: ['']
+          peso_actual: ['',[Validators.required, Validators.min(0)]],
+          peso_ideal: ['',[Validators.required, Validators.min(0)]],
+          p_grasa_corporal: ['',[Validators.required, Validators.min(0)]],
+          p_masa_muscular: ['',[Validators.required, Validators.min(0)]],
+          p_grasa_visceral: ['',[Validators.required, Validators.min(0)]],
+          peso_meta: ['',[Validators.required, Validators.min(0)]],
+          talla: ['',[Validators.required, Validators.min(0)]],
+          c_cintura: ['',[Validators.min(0)]],
+          imc: ['',[Validators.required, Validators.min(0)]],
+          edad_metabolica: ['',[Validators.min(0)]],
+          c_brazo_relaj: ['',[Validators.min(0)]],
+          c_cadera: ['',[Validators.min(0)]],
+          c_muneca: ['',[Validators.min(0)]]
         });
         break;
       case "historia_dietetica":
@@ -54,7 +55,7 @@ export class ConsultaForm{
           preferencia_alimen: [''],
           alimentos_no_gustan: [''],
           intolerancia_alergia: [''],
-          actividad_fisica: [''],
+          actividad_fisica: ['',[Validators.required]],
           alcohol: [''],
           tabaco: [''],
         });
@@ -94,26 +95,26 @@ export class ConsultaForm{
         break;
       case "datos_antropo":
           form = this.FB.group({
-          peso_actual: [''],
-          p_grasa_corporal: [''],
-          p_masa_muscular: [''],
-          p_grasa_visceral: [''],
-          c_cintura: [''],
-          imc: [''],
-          edad_metabolica: [''],
-          c_brazo_relaj: [''],
-          c_cadera: [''],
-          c_muneca: ['']
+          peso_actual: ['',[Validators.required, Validators.min(0)]],
+          p_grasa_corporal: ['',[Validators.required, Validators.min(0)]],
+          p_masa_muscular: ['',[Validators.required, Validators.min(0)]],
+          p_grasa_visceral: ['',[Validators.required, Validators.min(0)]],
+          c_cintura: ['',[Validators.min(0)]],
+          imc: ['',[Validators.required, Validators.min(0)]],
+          edad_metabolica: ['',[Validators.min(0)]],
+          c_brazo_relaj: ['',[Validators.min(0)]],
+          c_cadera: ['',[Validators.min(0)]],
+          c_muneca: ['',[Validators.min(0)]]
         });
         break;
       case "historia_dietetica":
           form = this.FB.group({
-          agua: [''],
-          actividad_fisica: [''],
-          observacion_menu_anterior: [''],
-          saciedad: [''],
+          agua: ['',[Validators.required]],
+          actividad_fisica: ['',[Validators.required]],
+          observacion_menu_anterior: ['',[Validators.required]],
+          saciedad: ['',[Validators.required]],
           alimentos_quiere: [''],
-          diagnostico_nutricional: [''],
+          diagnostico_nutricional: ['',[Validators.required]],
         });
         break;
       default:
