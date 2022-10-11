f1393654af15e3f2b00c36f2c7245a52cb29d359
Bizaarkq <isaac.bernab.01@gmail.com>
2022-10-10 18:43:05 -0600

    ✨ nut 200 agregando metodo para validacion de campos del formulario de recordatorio

--- src/app/views/components/consulta/recordatorio/recordatorio.component.ts
+++ src/app/views/components/consulta/recordatorio/recordatorio.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, OnChanges, OnInit, EventEmitter } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import recordatorioForm from './recordatorio.json';
+import { ValidarFormService } from 'src/app/services/general.service';
 @Component({
   selector: 'app-recordatorio',
   templateUrl: './recordatorio.component.html',
   styleUrls: ['./recordatorio.component.css']
 })
-export class RecordatorioComponent implements OnInit {
+export class RecordatorioComponent implements OnInit, OnChanges {
   @Input() formRecordatorio !: FormGroup;
+  @Input() realizarValidacion: boolean = false;
+  @Output() validacionForm = new EventEmitter<Object>();
 
-  camposRecordatorio = recordatorioForm;
+  camposRecordatorio:{ [key:string] : any} = recordatorioForm;
   
   constructor(private fb:FormBuilder) {}
 
@@ -17,10 +20,21 @@ export class RecordatorioComponent implements OnInit {
     this.createForm();
   }
 
+  ngOnChanges(): void {
+    if(this.realizarValidacion){
+      if(this.realizarValidacion){
+        this.validacionForm.emit({ 
+          'form': 'Recordatorio de 24 Horas', 
+          'campos': this.validandoForm()
+        });
+      }
+    }
+  }
+
   createForm():void{
     Object.entries(this.camposRecordatorio).forEach(([key, value]) => {
       this.formRecordatorio.addControl(key, this.fb.control('', 'validators' in  value ?
-      value.validators?.map(function (validator) {
+      value.validators?.map(function (validator:any) {
         if (!('params' in validator)) {
           return (Validators as any)[validator['type']];
         } else {
@@ -34,5 +48,20 @@ export class RecordatorioComponent implements OnInit {
   validarCampo( campo:string ){
     return this.formRecordatorio.controls[campo].errors && 
             this.formRecordatorio.controls[campo].touched
+  }
+
+  validandoForm():Object{
+    let data:any = ValidarFormService(this.formRecordatorio);
+    let campos:any = {};
+
+    if(data.incorrectos.length) campos.incorrectos = data.incorrectos.map((campo:any) => {
+        return this.camposRecordatorio[campo].label;
+      });
+
+    if(data.vacios.length) campos.vacios = data.vacios.map((campo:any) => {
+        return this.camposRecordatorio[campo].label;
+      });
+
+    return campos;
   }
 }
